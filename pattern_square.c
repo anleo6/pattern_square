b319c0254bc81fde38dc022574116bb4166e77e1
anleo6 <anleo6@users.noreply.github.com>
2019-02-21 22:22:46 -0800

    Fixed comments

--- pattern_square.c
+++ pattern_square.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+// Function returns number entered by user
 int enter_number();
 
 int main() 
 {
-    // we can do counter of lines from 0 to n - 1 and then downgrade it to 0
-    // so we can to do same thing with each line, when number position is equal
-    // from 0 to n - 1 and then downgrades to 0
+    // We can count lines from 0 to (n - 1) 
+    // and then smoothly back to 0.
+    // So, we can use the same scenario for indexes of lines.
     // 
-    // So, now we can think only about this part: 
+    // Thus, now we can think only about 1/4 of our matrix.
+    // It is top left corner for n = 4: 
     // | 4 4 4 4 --> lines
     // | 4 3 3 3
     // | 4 3 2 2
@@ -19,35 +21,41 @@ int main()
     int n = enter_number();
     printf("\n");
     
-    // All flags became 1 after middle of columns or lines
+    // Flags become 1 after middle of line or lines, respectively
     int col_flag = 0;
     int line_flag = 0;
     
-    // Temporary value it is printable number in our matrix
-    int temp = n;
-
+    // Number for print in our matrix
+    // First number will be n, therefore:
+    int print = n;
+    
+    // Start loops, values change later in code, 
+    // they depends on position in matrix
     for (int line = 0; line >= 0; ) {
         for (int col = 0; col >= 0; ) {
-            if (!col_flag && col && line && col <= line) temp--;
-            else if (col_flag && line && col < line) temp++;
+            if (!col_flag && col && line && (col <= line)) print--;
+            else if (col_flag && line && (col < line)) print++;
             
-            printf("%d ", temp);
+            printf("%d ", print);
             
-            // after middle of line start to reverse value of column
+            // In middle of line set flag
+            // to downgrade value of column
             if (col == n - 1) col_flag = 1;
             
             if (col_flag) col--;
             else col++;
         }
         
-        // after middle of line start to reverse value of line
+        // In middle of lines set flag
+        // to downgrade value of line
         if (line == n - 1) line_flag = 1;
         
         if (line_flag) line--;
         else line++;
         
-        // before next line set flag to 0 and temp back to n
-        temp = n;
+        // Before next line must set value for print 
+        // and reset flag
+        print = n;
         col_flag = 0;
         
         printf("\n");
